6ec16941d5f9e86e13231f905f5ba5f3c82e2441
mutedalien <chel_c@mail.ru>
2020-10-31 22:22:16 +0300

    week 1

--- W1/hello.py
+++ W1/hello.py
@@ -38,3 +38,45 @@ print(num)
 # 1,5 умножить на 10 в степени 2
 num = 1.5e2
 print(num)
+
+# Конвертация типов
+num = 150.2
+print(type(num))
+
+num = int(num)
+print(num, type(num))
+
+num = float(num)
+print(num, type(num))
+
+# Комплексные числа (complex)
+num = 14 + 1j
+
+print(type(num))
+print(num.real)
+print(num.imag)
+
+# Математика
+print(10 * 3 + 3)
+print(10 * (3 + 3))
+
+# print(100 / (100 % 1)) # Исключение
+
+# Побитовые операции
+x = 4
+y = 3
+
+print("Побитовое или: ", x | y)
+print("Побитовое исключающее или: ", x ^ y)
+print("Побитовое и: ", x & y)
+print("Битовый сдвиг влево: ", x << 3)
+print("Битовый сдвиг вправо: ", x >> 1)
+print("Инверсия битов: ", ~x)
+
+# Декартовые координаты (найти гипотенузу прямоугольного треугольника)
+x1, y1 = 0, 0
+x2 = 3
+y2 = 4
+
+distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+print(distance)
