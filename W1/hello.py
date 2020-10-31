8bdcaf992a17d09501ee76752639f4f8b809eb89
mutedalien <chel_c@mail.ru>
2020-10-31 21:37:56 +0300

    week 1

---
+++ W1/hello.py
@@ -0,0 +1,40 @@
+# Comment
+
+"""
+Многострочный
+комментарий
+"""
+
+print("Hello, world!") # Comment
+
+for i in range(4):
+    print(i)
+
+num = 13
+print(num)
+
+num = 0
+print(num)
+
+num = 10
+print(num)
+
+num = 13
+print(type(num))
+
+num = 13.4
+print(num)
+
+num = 0.0
+print(num)
+
+num = -10.2
+print(num)
+print(type(num))
+
+num = 100_000.000_001
+print(num)
+
+# 1,5 умножить на 10 в степени 2
+num = 1.5e2
+print(num)
