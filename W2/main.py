75d51ef08f663d67f64cf8ec6b9768c22110641b
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-11-03 14:42:41 +0300

    week 2

---
+++ W2/main.py
@@ -0,0 +1,13 @@
+# Списки
+
+empty_list = []
+empty_list = list()
+
+non_list = [None] * 10
+
+collections = ['list', 'tuple', 'dict', 'set']
+
+user_data = [
+    ['Elena', 4.4],
+    ['Andrey', 4.2]
+]
