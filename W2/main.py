1e634a7a75a97770f80b0bc7b520c30c7d9cd9a3
mutedalien <chel_c@mail.ru>
2020-11-05 20:42:51 +0300

    week 2

--- W2/main.py
+++ W2/main.py
@@ -12,8 +12,24 @@ user_data = [
     ['Andrey', 4.2]
 ]
 print(len(collections))
+# 4
 print(collections)
+# ['list', 'tuple', 'dict', 'set']
 print(collections[0])
+# list
 print(collections[-1])
+# set
 
 collections[3] = 'frozenset'
+print(collections)
+# ['list', 'tuple', 'dict', 'frozenset']
+
+range_list = list(range(10))
+print(range_list)
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(range_list[1:3])
+# [1, 2]
+print(range_list[3:])
+# [3, 4, 5, 6, 7, 8, 9]
+print(range_list[:5])
+# [0, 1, 2, 3, 4]
