95182577f0e6def029a9e398aeaae079e5dc0240
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-11-05 20:34:36 +0300

    week 2

--- W2/main.py
+++ W2/main.py
@@ -11,3 +11,9 @@ user_data = [
     ['Elena', 4.4],
     ['Andrey', 4.2]
 ]
+print(len(collections))
+print(collections)
+print(collections[0])
+print(collections[-1])
+
+collections[3] = 'frozenset'
