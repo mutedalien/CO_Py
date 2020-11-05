d394025ad7317871cd773413a8ce1e41e2ee99cb
mutedalien <chel_c@mail.ru>
2020-11-05 20:50:53 +0300

    week 2

--- W2/main.py
+++ W2/main.py
@@ -1,4 +1,4 @@
-# Списки
+# СПИСКИ
 
 empty_list = []
 empty_list = list()
@@ -33,3 +33,29 @@ print(range_list[3:])
 # [3, 4, 5, 6, 7, 8, 9]
 print(range_list[:5])
 # [0, 1, 2, 3, 4]
+print(range_list[::-1])
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+print(range_list[5:1:-1])
+# [5, 4, 3, 2]
+range_list[:] is range_list
+# False
+
+# ИТЕРАЦИИ
+
+collections = ['list', 'tuple', 'dict', 'set']
+for collection in collections:
+    print('Learning {}...'.format(collection))
+"""""
+Learning list...
+Learning tuple...
+Learning dict...
+Learning set...
+"""""
+for idx, collection in enumerate(collections):
+    print('#{} {}'.format(idx, collection))
+"""""
+#0 list
+#1 tuple
+#2 dict
+#3 set
+"""""
