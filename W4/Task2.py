0aad09febc7ed6b13e6bbb66888b82c8801d3ad3
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-11-12 11:55:08 +0300

    week 4

---
+++ W4/Task2.py
@@ -0,0 +1,15 @@
+class Value:
+    def __init__(self):
+        self.money = 0
+
+    def __set__(self, instance, value):
+        self.money = value
+
+    def __get__(self, instance, owner):
+        return self.money - (self.money * instance.commission)
+
+class Account:
+    amount = Value()
+
+    def __init__(self, commission):
+        self.commission = commission
