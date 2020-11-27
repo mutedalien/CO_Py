5ada4f9f46dbd20e243d65aad744c722ddfd9720
mutedalien <chel_c@mail.ru>
2020-11-27 23:18:45 +0200

    week 5

--- W5/sockets.py
+++ W5/sockets.py
@@ -53,3 +53,136 @@ import socket
 
 with socket.create_connection(("127.0.0.1", 10001)) as sock:
     sock.sendall("ping".encode("utf8"))
+
+# Таймауты и обработка сетевых ошибок
+
+# сервер
+import socket
+
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        conn.settimeout(5)  # timeout := None|0|gt 0
+        with conn:
+            while True:
+                try:
+                    data = conn.recv(1024)
+                except socket.timeout:
+                    print("close connection by timeout")
+                    break
+
+                if not data:
+                    break
+                print(data.decode("utf8"))
+# создание сокета, таймауты и обработка ошибок
+# клиент
+import socket
+
+with socket.create_connection(("127.0.0.1", 10001), 5) as sock:
+    # set socket read timeout
+    sock.settimeout(2)
+    try:
+        sock.sendall("ping".encode("utf8"))
+    except socket.timeout:
+        print("send data timeout")
+    except socket.error as ex:
+        print("send data error:", ex)
+
+# обработка нескольких соединений одновременно
+
+import socket
+
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        print("connected client:", addr)
+        # процесс или поток для обработки соединения
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data.decode("utf8"))
+
+# обработка нескольких соединений одновременно, потоки
+
+import socket
+import threading
+
+def process_request(conn, addr):
+    print("connected client:", addr)
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data.decode("utf8"))
+
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        th = threading.Thread(target=process_request, args=(conn, addr))
+        th.start()
+
+# обработка нескольких соединений одновременно, процессы и потоки
+import socket
+
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+    # создание нескольких процессов
+    while True:
+        # accept распределится "равномерно" между процессами
+        conn, addr = sock.accept()
+        # поток для обработки соединения
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data.decode("utf8"))
+# обработка нескольких соединений одновременно, процессы и потоки
+
+import socket
+import threading
+import multiprocessing
+
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+
+    workers_count = 3
+    workers_list = [multiprocessing.Process(target=worker, args=(sock,))
+                    for _ in range(workers_count)]
+
+    for w in workers_list:
+        w.start()
+
+    for w in workers_list:
+        w.join()
+
+
+# обработка нескольких соединений одновременно, процессы и потоки
+
+def worker(sock):
+    while True:
+        conn, addr = sock.accept()
+        print("pid", os.getpid())
+        th = threading.Thread(target=process_request, args=(conn, addr))
+        th.start()
+
+
+def process_request(conn, addr):
+    print("connected client:", addr)
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data.decode("utf8"))
