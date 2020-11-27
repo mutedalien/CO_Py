bb78d0d42bcde5b4fbd6d324f59cc8d45888435a
mutedalien <chel_c@mail.ru>
2020-11-27 23:38:27 +0200

    week 5

--- W5/asyncio.py
+++ W5/asyncio.py
@@ -168,3 +168,131 @@ def the_same_chain(x_iterable, y_iterable):
 # 3
 # 4
 # 5
+
+# asyncio, async def / await; PEP 492 Python3.5
+
+import asyncio
+
+async def hello_world():
+    while True:
+        print("Hello World!")
+        await asyncio.sleep(1.0)
+
+# >>> loop = asyncio.get_event_loop()
+# >>> loop.run_until_complete(hello_world())
+# >>> loop.close()
+# Hello World!
+# Hello World!
+# ...
+
+# asyncio, tcp сервер
+
+import asyncio
+
+
+async def handle_echo(reader, writer):
+    data = await reader.read(1024)
+    message = data.decode()
+    addr = writer.get_extra_info("peername")
+    print("received %r from %r" % (message, addr))
+    writer.close()
+
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, "127.0.0.1", 10001, loop=loop)
+server = loop.run_until_complete(coro)
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
+# asyncio, tcp клиент
+
+import asyncio
+
+
+async def tcp_echo_client(message, loop):
+    reader, writer = await asyncio.open_connection("127.0.0.1", 10001, loop=loop)
+
+    print("send: %r" % message)
+    writer.write(message.encode())
+    writer.close()
+
+
+loop = asyncio.get_event_loop()
+message = "hello World!"
+loop.run_until_complete(tcp_echo_client(message, loop))
+loop.close()
+
+### asyncio.Future, аналог concurrent.futures.Future
+
+import asyncio
+
+async def slow_operation(future):
+    await asyncio.sleep(1)
+    future.set_result("Future is done!")
+
+# >>> loop = asyncio.get_event_loop()
+# >>> future = asyncio.Future()
+# >>> asyncio.ensure_future(slow_operation(future))
+# >>>
+# >>> loop.run_until_complete(future)
+# >>> print(future.result())
+# Future is done!
+# >>> loop.close()
+
+
+### asyncio.Task, запуск нескольких корутин
+
+import asyncio
+
+
+async def sleep_task(num):
+    for i in range(5):
+        print(f"process task: {num} iter: {i}")
+        await asyncio.sleep(1)
+
+    return num
+
+# ensure_future or create_task
+# >>> loop = asyncio.get_event_loop()
+#
+# >>> task_list = [loop.create_task(sleep_task(i)) for i in range(2)]
+# >>> loop.run_until_complete(asyncio.wait(task_list))
+#
+# >>> loop.run_until_complete(loop.create_task(sleep_task(3)))
+# >>> loop.run_until_complete(asyncio.gather(
+# >>> sleep_task(10),
+# >>> sleep_task(20),
+# >>> ))
+
+# loop.run_in_executor, запуск в отдельном потоке
+
+import asyncio
+from urllib.request import urlopen
+
+# a synchronous function
+def sync_get_url(url):
+   return urlopen(url).read()
+
+async def load_url(url, loop=None):
+    future = loop.run_in_executor(None, sync_get_url, url)
+    response = await future
+    print(len(response))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(load_url("https://google.com", loop=loop))
+
+
+##### Библиотеки asyncio
+# https://github.com/aio-libs
+# aiohttp
+# https://github.com/aio-libs/aiohttp
+# aiomysql
+# https://github.com/aio-libs/aiomysql
+# aiomcache
+# https://github.com/aio-libs/aiomcache
+# https://docs.python.org/3/library/asyncio.html
